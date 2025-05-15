a9f230629a82423342f51353d23920b361884e7f
bennett <bennett_asia@outlook.com>
2025-05-16 00:12:14 +0800

    [feat](app): 新增去除B站直播和主播玩贴片

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -6,7 +6,7 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '局部广告',
+      name: '直播间-局部广告',
       activityIds: 'com.bilibili.bililive.room.ui.roomv3.LiveRoomActivityV3',
       rules: [
         {
@@ -24,6 +24,15 @@ export default defineGkdApp({
           matchTime: 5000,
           matches:
             'ImageView[id="tv.danmaku.bili:id/iv_pop_rank_guide_card_close"][clickable=true]',
+          snapshotUrls: ['https://i.gkd.li/i/20230038'],
+        },
+        {
+          key: 2,
+          name: '直播间-和主播一起玩',
+          fastQuery: true,
+          matchTime: 5000,
+          matches:
+            'ImageView[id="tv.danmaku.bili:id/live_play_together_close"]',
           snapshotUrls: ['https://i.gkd.li/i/20230038'],
         },
       ],
