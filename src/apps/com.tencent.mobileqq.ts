609bd986c7aee3ec3989f273560ad1c8210d65d8
bennett <bennett_asia@outlook.com>
2025-11-17 01:04:57 +0800

    [feat](app): QQ 频道帖子内的广告

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -27,5 +27,28 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '局部广告-频道帖子夹杂的广告',
+      activityIds:
+        'com.tencent.mobileqq.guild.base.QPublicFragmentActivityForMainWebActivity',
+      rules: [
+        {
+          key: 0,
+          name: '频道帖子夹杂广告',
+          fastQuery: true,
+          matches: '[id="com.tencent.mobileqq:id/nz1"][text="广告"]',
+          snapshotUrls: ['https://i.gkd.li/i/23558759'],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          name: '选择关闭广告',
+          fastQuery: true,
+          matches: 'TextView[text="关闭此条广告"]',
+          snapshotUrls: ['https://i.gkd.li/i/23569591'],
+        },
+      ],
+    },
   ],
 });
